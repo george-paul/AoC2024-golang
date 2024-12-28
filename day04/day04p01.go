fa190551d7ac544f1491aa262d1237ed1b944010
George Paul <g.paul759@gmail.com>
2024-12-28 20:39:07 +0530

    Day04 Part01

---
+++ day04/day04p01.go
@@ -0,0 +1,82 @@
+package day04
+
+import "errors"
+
+func moveCellIndex(dir uint8, rIdx int, cIdx int) (newRIdx int, newCIdx int, err error) {
+	switch dir {
+	case cul:
+		newRIdx = rIdx - 1
+		newCIdx = cIdx - 1
+	case cuu:
+		newRIdx = rIdx - 1
+		newCIdx = cIdx
+	case cur:
+		newRIdx = rIdx - 1
+		newCIdx = cIdx + 1
+	case cll:
+		newRIdx = rIdx
+		newCIdx = cIdx - 1
+	case crr:
+		newRIdx = rIdx
+		newCIdx = cIdx + 1
+	case cdl:
+		newRIdx = rIdx + 1
+		newCIdx = cIdx - 1
+	case cdd:
+		newRIdx = rIdx + 1
+		newCIdx = cIdx
+	case cdr:
+		newRIdx = rIdx + 1
+		newCIdx = cIdx + 1
+	}
+	if newRIdx < 0 || newCIdx < 0 || newRIdx >= dimH || newCIdx >= dimW {
+		err = errors.New("Out of Bounds")
+		newCIdx = cIdx
+		newRIdx = rIdx
+	} else {
+		err = nil
+	}
+	return
+}
+
+func genPresList(cells Cells, rIdx int, cIdx int) (newPresList PresList, noYes int) {
+	newPresList = initialPres
+	thisCell := cells[rIdx][cIdx]
+	noYes = 0 // number of ye's
+	for idx, dir := range dirs {
+		newRIdx, newCIdx, err := moveCellIndex(dir, rIdx, cIdx)
+		if err != nil {
+			continue
+		}
+		adjCell := cells[newRIdx][newCIdx]
+		if adjCell.letter == thisCell.letter-1 {
+			if adjCell.pres[idx] == ye {
+				noYes += 1
+			}
+			newPresList[idx] = adjCell.pres[idx]
+		}
+	}
+	return
+}
+
+func d04p01(cells Cells) int {
+	total := 0
+	for _, letter := range [4]uint8{2, 3, 4} {
+		for rIdx, cellRow := range cells {
+			for cIdx, cell := range cellRow {
+				if cell.letter == letter {
+					switch cell.letter {
+					case 2, 3:
+						newCell := cell
+						newCell.pres, _ = genPresList(cells, rIdx, cIdx)
+						cells[rIdx][cIdx] = newCell
+					case 4:
+						_, noYes := genPresList(cells, rIdx, cIdx)
+						total += noYes
+					}
+				}
+			}
+		}
+	}
+	return total
+}
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
-import (
-	"AoC_2024/day03"
-)
+import "AoC_2024/day04"
 
 func main() {
 	// day01.Day01Main()
 	// day02.Day02Main()
-	day03.Day03Main()
+	// day03.Day03Main()
+	day04.Day04Main()
 }
