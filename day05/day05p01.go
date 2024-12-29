3e10d6b1d831ffcc8425870b6405191139d514e5
George Paul <g.paul759@gmail.com>
2024-12-29 12:08:58 +0530

    Day05 Part02

--- day05/day05p01.go
+++ day05/day05p01.go
@@ -9,10 +9,10 @@ func getMiddleNumber(update []int) int {
 // returns false, a, b if update is not in order and orderings[a][b] was violated
 func checkUpdateOrder(orderings Orderings, update []int) (bool, int, int) {
 	beforeNums := []int{}
-	for _, num := range update {
-		for _, beforeNum := range beforeNums {
+	for nIdx, num := range update {
+		for bnIdx, beforeNum := range beforeNums {
 			if !orderings[beforeNum][num] {
-				return false, beforeNum, num
+				return false, bnIdx, nIdx
 			}
 		}
 		beforeNums = append(beforeNums, num)
--- day05/day05p02.go
+++ day05/day05p02.go
@@ -1,13 +1,25 @@
 package day05
 
+func swapNumsInUpdate(update []int, a int, b int) []int {
+	newUpdate := update
+	temp := update[a]
+	update[a] = update[b]
+	update[b] = temp
+	return newUpdate
+}
+
 func d05p02(orderings Orderings, updates Updates) int {
 	total := 0
 	for _, update := range updates {
-		isInOrder, _, _ := checkUpdateOrder(orderings, update)
-		if isInOrder {
-			total += getMiddleNumber(update)
+		isInOrder, a, b := checkUpdateOrder(orderings, update)
+		for !isInOrder {
+			update = swapNumsInUpdate(update, a, b)
+			isInOrder, a, b = checkUpdateOrder(orderings, update)
+			if isInOrder {
+				total += getMiddleNumber(update)
+				break
+			}
 		}
 	}
-
 	return total
 }
