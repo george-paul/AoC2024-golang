5e6c0eff271d2c4856afef6210797fc3aabb5212
George Paul <g.paul759@gmail.com>
2025-01-01 13:23:44 +0530

    trying codespaces

---
+++ day06/day06p01.go
@@ -0,0 +1,15 @@
+package day06
+
+func isInBounds(r int, c int) bool {
+	rInBounds := (r >= 0) && (r < dimH)
+	cInBounds := (c >= 0) && (c < dimW)
+	return rInBounds && cInBounds
+}
+
+func d06p01(grid Grid, begR int, begC int) int {
+	total := 0
+	r := begR
+	c := begC
+	for 
+	return total
+}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "AoC_2024/day05"
+import "AoC_2024/day06"
 
 func main() {
 	// day01.Day01Main()
 	// day02.Day02Main()
 	// day03.Day03Main()
 	// day04.Day04Main()
-	day05.Day05Main()
+	// day05.Day05Main()
+	day06.Day06Main()
 }
